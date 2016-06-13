247df353cd207aca403d5c111f07cc5927f70663
Austin Peterson <austin@akpwebdesign.com>
2016-06-13 10:18:28 -0500

    Update to new plugin/context systems.

--- main.js
+++ main.js
@@ -3,19 +3,16 @@ const chance = new (require('chance'))();
 
 class AyyLmao extends global.AKP48.pluginTypes.MessageHandler {
   constructor(AKP48) {
-    super('AyyLmao', AKP48);
+    super(AKP48, 'AyyLmao');
   }
 }
 
-AyyLmao.prototype.handleMessage = function (message, context, res) {
-  global.logger.stupid(`${this._pluginName}: Received message.`);
-  if(message.toLowerCase().includes('ayy')) {
-    context.noPrefix = true;
+AyyLmao.prototype.handleMessage = function (context) {
+  global.logger.stupid(`${this.name}: Received message.`);
+  if(context.text().toLowerCase().includes('ayy')) {
+    context.setCustomData('noPrefix', true);
     this._AKP48.sendMessage((chance.bool({likelihood: 25}) ? 'ayy lamo' : 'ayy lmao'), context);
-    res(false);
   }
 };
 
 module.exports = AyyLmao;
-module.exports.type = 'MessageHandler';
-module.exports.pluginName = 'ayy-lmao';
